9e33913bfabd3195db1adc7d52b47474f7c17dac
shamimfstack <shamimspy@gmail.com>
2023-09-17 21:12:45 +0600

    starter template

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-7xl bg-rose-600">Vite + React</h1>
+      <div className="stats shadow">
+  
+  <div className="stat">
+    <div className="stat-title">Total Page Views</div>
+    <div className="stat-value">89,400</div>
+    <div className="stat-desc">21% more than last month</div>
+  </div>
+  
+</div>
+    </>
+  )
+}
+
+export default App
